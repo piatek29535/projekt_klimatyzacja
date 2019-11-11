6a30f5d57aea6108cd6186dd542c7b102c57181b
piatek29535 <adamex12343@gmail.com>
2019-11-11 12:14:52 +0100

    added server code and connected with sensor

--- main/main.ino
+++ main/main.ino
@@ -1,23 +1,56 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 #include "DHTesp.h"
 
+ESP8266WebServer server(80);
 DHTesp dht;
 
-void setup()
-{
+const char* ssid = "ssid";
+const char* password = "hasło";
+
+void setup(){
   Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.print("\n Adres WiFi: ");
+  Serial.print(WiFi.localIP());
+  Serial.println();
+
+  server.on("/",helloWorld);
+  server.onNotFound(notFound);
+
+  server.begin();
+  Serial.print("Serwer działa");
+  
   dht.setup(14, DHTesp::DHT11); //D5
 }
 
-void loop()
-{
+void loop(){
+    server.handleClient();
+}
+
+void helloWorld(){
+  server.send(200,"text/plain", printTemperature());
+}
+
+void notFound(){
+  server.send(404, "text/plain", "Nie znaju");
+}
+
+//use it later 
+String printTemperature(){
   delay(dht.getMinimumSamplingPeriod());
 
   float temperature = dht.getTemperature();
- 
-  Serial.print("Temperatura w pokoju wynosi :");
-  Serial.print("\t\t");
-  Serial.print(temperature, 1);
-  Serial.println();
 
-  delay(1000);
+  String message = "Temperatura wynosi: ";
+  message += temperature;
+  message += " stopni Celsjusza";
+
+  return message;
 }
